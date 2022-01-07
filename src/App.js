8f39fb16f380de2f97551fe9cb7220ce4403eb94
Edmilson Mello <edmilson.mello@hotmail.com.br>
2022-01-07 11:07:12 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { Cta, Brand, Navbar } from './components';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <Cta />
+            <Blog />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
